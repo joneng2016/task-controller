a2c809550ce6a12fd54cf5e97eef287d67a2cd2f
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2025-04-17 22:33:00 -0300

    efetuando upload do projeto task controller

---
+++ src/test/java/com/example/task_controller/TaskControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
